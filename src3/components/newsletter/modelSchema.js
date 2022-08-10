5f05959e83bddcdf3393a83df9e57e0146c38ae4
ivandcdesign <ivandcdesign@gmail.com>
2022-08-09 21:00:37 -0400

    v1.0

--- src3/components/newsletter/modelSchema.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require("mongoose");
-
-const mySchema = Schema({
-  email: { type: String, required: true },
-});
-
-const modelSchema = model("Newsletter", mySchema);
-
-module.exports = modelSchema;
--- src3/network/router.js
+++
@@ -1,9 +0,0 @@
-const newsletter = require("../components/newsletter/network");
-// const form = require("../components/form/network");
-
-const router = (server) => {
-  server.use("/api/newsletter/", newsletter);
-  // server.use("/api/form/", form);
-};
-
-module.exports = router;
